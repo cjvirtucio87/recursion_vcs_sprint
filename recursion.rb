9b2da9d59468ccd61338f80f39f3041805ca154c
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-12 09:41:55 -0700

    factorial and digit sum

---
+++ recursion.rb
@@ -0,0 +1,29 @@
+def factorial_r(num)
+  return 1 if num == 1
+  return num * factorial_r(num - 1)
+end
+
+
+def factorial_i(num)
+  product = 1
+  (1..num).each do |n|
+    product *= n
+  end
+  product
+end
+
+
+def digit_sum_r(num)
+  return num if num.to_s.length == 1
+
+  num.to_s[0].to_i + digit_sum_r(num.to_s[1..-1].to_i)
+end
+
+def digit_sum_i(num)
+  sum = 0
+  
+  num.to_s.split('').map(&:to_i).reduce(&:+)
+end
+
+p digit_sum_r(0)
+p digit_sum_i(0)
